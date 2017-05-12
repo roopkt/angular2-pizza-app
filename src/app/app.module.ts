b5d7726144eff92bbd566db5b743857f77476d89
roopkt <roopkt@gmail.com>
2017-05-12 16:54:19 -0400

    Created angular pizza app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PizzaCreatorComponent } from './pizza-creator/pizza-creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaCreatorComponent
   ],
   imports: [
     BrowserModule,
